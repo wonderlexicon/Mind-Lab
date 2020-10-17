3ab45e4f0b1e9b308e185cb1903162a18b716f9f
wonderlexicon <nh756@nyu.edu>
2020-10-17 11:53:42 -0400

    baseline first draft of work in process v1 of MindLab

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-
 //  * @name Slider
 //  * @description Move the sliders to control the R, G, B values of the background.
 //  */
-let rSlider, gSlider, bSlider;
+
 
 function setup() {
   // create canvas
@@ -12,11 +11,11 @@ function setup() {
 
   // create sliders
   rSlider = createSlider(0, 255, 100);
-  rSlider.position(800, 3540);
+  rSlider.position(300, 4250);
   gSlider = createSlider(0, 255, 0);
-  gSlider.position(800, 3520);
+  gSlider.position(300, 4300);
   bSlider = createSlider(0, 255, 255);
-  bSlider.position(800, 3500);
+  bSlider.position(300, 4350);
 }
 
 function draw() {
@@ -24,64 +23,104 @@ function draw() {
   const g = gSlider.value();
   const b = bSlider.value();
   background(r, g, b);
-  text('red', rSlider.x * 2 + rSlider.width, 35);
-  text('green', gSlider.x * 2 + gSlider.width, 65);
-  text('blue', bSlider.x * 2 + bSlider.width, 95);
+  text('THINK', rSlider.x * 2 + rSlider.width, 35);
+  text('FEEL', gSlider.x * 2 + gSlider.width, 65);
+  text('ACT', bSlider.x * 2 + bSlider.width, 95);
 }
 
+let sliderPos = ("rSlider.position", "gSlider.position", "bSlider.position");
+// let rSlider.position, gSlider.position, bSlider.position = sliderPos;
+
+// const myCanvas = createCanvas
+// const mySliderPos = sliderPos
+//Set the parent of the canvas to an HTML element by it's ID value  - from Craig's response on-->
+// myCanvas.parent("canvas-container");
+// mySliderPos.parent("sliders-container");
+
 const circumstances = [
-    "I see the headlines in the newspaper",
-     "my alarm goes off in the morning",
-      "I have a birthday",
-       "it's raining"];
-       //how do i make a new sentence for each circumstance show up w a random emotion?""
+  "I see the headlines in the newspaper",
+  "my alarm goes off in the morning",
+  "I have a birthday",
+  "it's raining"
+];
+//how do i make a new sentence for each circumstance show up w a random emotion?""
 // I feel put in json options - based on the mood the p5 changes color 
-window.addEventListener('load', function() {
-    
-    console.log('page is loaded');
+window.addEventListener('load', function () {
 
-   //Load the json data file 
-   let myEmotionsSet; 
-   fetch('moods.json')
-   .then(response => response.json())
-   .then(data => {
-        console.log(data);
-        //Do something with 'data'
+  console.log('page is loaded');
+
+  //Load the json data file 
+  let myEmotionsSet;
+  fetch('moods.json')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      //Do something with 'data'
       //  document.getElementById("myDiv").innerHTML=data.emotions;
-        shuffle (data.emotions);
-        myEmotionsSet=data.emotions;
-        document.getElementById("feeling-input").value=data.emotions.slice(0,1);
-   })
+      shuffle(data.emotions);
+      myEmotionsSet = data.emotions;
+      document.getElementById("random-feeling-input").value = data.emotions.slice(0, 1);
+    })
 
-   function shuffle(array) {
+  function shuffle(array) {
     array.sort(() => Math.random() - 0.5);
   }
   // source https://javascript.info/task/shuffle
-  
-   
+
+
   let randomButton = document.getElementById("random-button")
+  console.log("button");
   randomButton.addEventListener('click', function (e) {
-      console.log ("clickedRandom");
-      shuffle (myEmotionsSet);
-      document.getElementById("feeling-input").value=myEmotionsSet.slice(0,1);
+    console.log("clickedRandom");
+    shuffle(myEmotionsSet);
+    document.getElementById("random-feeling-input").value = myEmotionsSet.slice(0, 1);
+
 
   });
-//    if (yesNum > 2) {
-//     yesNum = 0;
-//  } 
 
 })
 
+function BackToTop() {
+  console.log("i clicked back to top");
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+function yesFunction() {
+  document.getElementById("yesClick").innerHTML =
+    "Great! You have just reinforced a positive pattern and strenthened your understanding of a causal connection that you can apply to other situations in your life! If you're ready for a bigger challenge, explore an unpleasant thought or go deeper into your exploration of a random emotion.";
+  // if (document.getElementById("yesClick").innerHTML.length === 0) {
+  //     document.getElementById("yesClick").innerHTML =
+  //         "Great! You have just reinforced a positive pattern and strenthened your understanding of a causal connection that you can apply to other situations in your life! If you're ready for a bigger challenge, explore an unpleasant thought or go deeper into your exploration of a random emotion.";
+  // } else {
+  //     document.getElementById("yesClick").
+  //     innerHTML = "";
+  // }
+}
+
+function noFunction() {
+  document.getElementById("yesClick").innerHTML =
+    "Great! You just discovered valuable information about what does not work for you. Try playing again, perhaps exploring a different thought/feeling/action combination. Before you know it you'll see for yourself the incredible results conscious thinking can create!";
+  // if (document.getElementById("noClick").innerHTML.length === 0) {
+  //     document.getElementById("noClick").innerHTML =
+  //         "Great! You just discovered valuable information about what does not work for you. Try playing again, perhaps exploring a different thought/feeling/action combination. Before you know it you'll see for yourself the incredible results conscious thinking can create!";
+  // } else {
+  //     document.getElementById("noClick").innerHTML = "";
+  // }
+}
 
 
 
+//    if (yesNum > 2) {
+//     yesNum = 0;
+//  } 
 // let yesAnswers = ["great", "super", "amazing"];
 // let yesNum = 0;
 
 //    .catch(error => {
 //        console.log("Error!!! : " + error);
 //    })
-
 
 // })
 // function setup(){
@@ -92,3 +131,70 @@ window.addEventListener('load', function() {
 
 //     function draw(){
 //     script
+
+function replaceUserResultsText() {
+  document.getElementById("userResults").innerHTML = document.getElementById(
+      "results-input")
+    .value;
+}
+
+function onResultButtonClicked() {
+  revealReviewSection();
+  replaceUserResultsText();
+}
+
+function revealReviewSection() {
+  if (allSectionsFilled()) {
+    document.getElementById("the-big-picture").removeAttribute("hidden");
+  }
+
+
+}
+
+function allSectionsFilled() {
+  return (
+    document.getElementById("userResults").innerHTML !== "" &&
+    document.getElementById("userActions").innerHTML !== "" &&
+    document.getElementById("userFeel").innerHTML !== "" &&
+    document.getElementById("userThought").innerHTML !== ""
+  );
+}
+
+// THOUGHT
+function replaceUserThoughtText() {
+  document.getElementById("userThought").innerHTML = document.getElementById(
+      "thought-input")
+    .value;
+}
+
+function replaceUserThoughtWithDropdown() {
+  console.log('dropdown')
+  let thoughtList = document.getElementById(
+    "thought-list");
+  document.getElementById("userThought").innerHTML = (
+    thoughtList.options[thoughtList.selectedIndex].text);
+
+}
+
+// FEEL
+function showRandomContainerIfDropdownSelected() {
+  console.log("showRandomFunction")
+  if (document.getElementById("feelingsList").value === "Random") {
+    document.getElementById("random-container").removeAttribute("hidden");
+  } else {
+    document.getElementById("random-container").hidden = true;
+  }
+}
+
+function onFeelingsDropdownChanged() {
+  let feelList = document.getElementById(
+    "feelingsList");
+  // if (feelList.value == "Random"){
+    showRandomContainerIfDropdownSelected();
+  // } 
+  if (feelList.value !== "Random"){
+     document.getElementById("userFeel").innerHTML =
+     (
+      feelList.options[feelList.selectedIndex].text);
+  }
+}
